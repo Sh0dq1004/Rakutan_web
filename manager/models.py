3ad392fe0269ac2f25ab78e1775264cf8f137e8c
Sh0dq1004 <atlantis.muro1004@gmail.com>
2025-03-06 12:48:06 +0900

    models has been changed

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class subject:
+    name=models.CharField(max_length=32)
+    teaher=models.CharField(max_length=16)
+    grade=models.IntegerField()
+    term=models.IntegerField()
+    major=models.CharField(max_length=8)
+    eval=models.IntegerField()
